fbbfcaf70daa1142237669bcb2b938a496d68c58
Helder <helder2006hr@gmail.com>
2026-08-18 18:12:04 +0200

    feat(spotlights): add 7 spotlights

--- src/sdps/frontend/components/entities/spotlight_rig.py
+++ src/sdps/frontend/components/entities/spotlight_rig.py
@@ -1,6 +1,7 @@
 from sdps.config import SPOTLIGHT_COLOR
 from sdps.frontend.components.shape import Shape
 
+N_SPOTLIGHTS = 7
 
 class SpotlightRig:
     def __init__(self, display, width, height):
@@ -12,3 +13,12 @@ class SpotlightRig:
         x = self.display.get_width() / 2 - self.width / 2
         shape = Shape(self.display)
         shape.draw_rectangle(x, y, self.width, self.height, SPOTLIGHT_COLOR)
+
+        spacing = self.width / N_SPOTLIGHTS
+        w = 30
+        h = 45
+        for i in range(N_SPOTLIGHTS):
+            c_x = x + spacing * (i + 0.5)
+            rect_x = c_x - w / 2
+            rect_y = y + self.height
+            shape.draw_rectangle(rect_x, rect_y, w, h, SPOTLIGHT_COLOR)
