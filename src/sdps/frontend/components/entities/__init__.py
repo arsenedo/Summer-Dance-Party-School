f32e4718fca71ee438a92b967ba8b21bb725ee2b
Helder <helder2006hr@gmail.com>
2026-08-19 12:35:42 +0200

    feat(confetti-cannon): throws confetti

--- src/sdps/frontend/components/entities/__init__.py
+++ src/sdps/frontend/components/entities/__init__.py
@@ -1,4 +1,6 @@
+from .confetti_cannon import ConfettiCannon
+from .particles import FloatingParticle, Particle
 from .spotlight import Spotlight
 from .spotlight_rig import SpotlightRig
 
-__all__ = ["Spotlight", "SpotlightRig"]
+__all__ = ["Spotlight", "SpotlightRig", "ConfettiCannon", "Particle", "FloatingParticle"]
--- src/sdps/frontend/engine.py
+++ src/sdps/frontend/engine.py
@@ -1,7 +1,20 @@
+from random import choice, randint, uniform
+
 import pygame
 
-from sdps.config import CONFETTI_PADDING_POS, CURTAINS_TIME, DARKNESS_ALPHA, FPS, SCREEN_HEIGHT, SCREEN_WIDTH
+from sdps.config import (
+    CONFETTI_PADDING_POS,
+    CURTAINS_TIME,
+    DARKNESS_ALPHA,
+    FPS,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+)
 from sdps.frontend.components.entities.confetti_cannon import ConfettiCannon
+from sdps.frontend.components.entities.particles import (
+    FloatingParticle,
+    Particle,
+)
 from sdps.frontend.components.entities.spotlight_rig import SpotlightRig
 from sdps.frontend.components.scene.curtains import Curtains
 from sdps.frontend.components.scene.floor import Floor
@@ -17,6 +30,11 @@ class Engine:
         self.shape = Shape(self.screen, 20)
         self.background = pygame.Surface(self.screen.get_size())
         self.curtains_speed = (SCREEN_WIDTH / 2) / (CURTAINS_TIME * 1000)
+
+        self.particle_group = pygame.sprite.Group()
+        self.floating_particle_timer = pygame.event.custom_type()
+        pygame.time.set_timer(self.floating_particle_timer, 10)
+
         pygame.display.set_caption('Summer Dance Party School')
 
         self._init_scene()
@@ -30,8 +48,11 @@ class Engine:
         self.spotlight_rig = SpotlightRig(self.screen, self.screen.get_width(), 20,
                                           self.floor.y_offset + self.floor.height / 2)
         self.darkness = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
-        self.left_cannon = ConfettiCannon(self.screen, CONFETTI_PADDING_POS, SCREEN_HEIGHT - CONFETTI_PADDING_POS, 135)
-        self.right_cannon = ConfettiCannon(self.screen, SCREEN_WIDTH - CONFETTI_PADDING_POS, SCREEN_HEIGHT - CONFETTI_PADDING_POS, 45)
+        self.left_cannon = ConfettiCannon(self.screen, CONFETTI_PADDING_POS,
+         SCREEN_HEIGHT - CONFETTI_PADDING_POS, 135)
+        self.right_cannon = ConfettiCannon(self.screen,
+                                           SCREEN_WIDTH - CONFETTI_PADDING_POS,
+                                           SCREEN_HEIGHT - CONFETTI_PADDING_POS, 45)
 
     def run(self):
         start_ticks = pygame.time.get_ticks()
@@ -47,8 +68,17 @@ class Engine:
                         self.spotlight_rig.toggle(i)
                     elif key == pygame.K_q:
                         self.left_cannon.shoot()
+                        x = self.left_cannon.x + self.left_cannon.body_size / 2
+                        y = self.left_cannon.y - self.left_cannon.body_size - self.left_cannon.barrel_length / 2
+                        self._spawn_particles(1000, (x, y), 1)
                     elif key == pygame.K_w:
                         self.right_cannon.shoot()
+                        x = self.right_cannon.x - self.right_cannon.body_size / 2
+                        y = self.right_cannon.y - self.right_cannon.body_size - self.right_cannon.barrel_length / 2
+                        self._spawn_particles(1000, (x, y), -1)
+                elif event.type == self.floating_particle_timer:
+                    #self._spawn_floating_particle()
+                    pass
 
             self.screen.blit(self.background, (0, 0))
             self.floor.draw(self.screen)
@@ -71,7 +101,35 @@ class Engine:
 
                 self.curtains.draw(self.screen, x)
 
+            dt = self.clock.tick() / 1000
+
+            self.particle_group.draw(self.screen)
+            self.particle_group.update(dt)
+
             pygame.display.flip()
             self.clock.tick(FPS)
 
         pygame.quit()
+
+    def _spawn_particles(self, n: int, pos: tuple[int, int], side: int):
+        """spawn particles for confettis
+
+        Args:
+            n (int): bumber of particles to spawn
+            pos (tuple[int, int]): position to spawn particles
+            side (int): direction to throw particles (-1: to the left, 1: to the right)
+        """
+        for _ in range(n):
+            color = choice(("red", "green", "blue"))
+            direction = pygame.math.Vector2(uniform(side, 0), uniform(-1, 0))
+            # direction = direction.normalize()
+            speed = randint(1, 1500)
+            Particle(self.particle_group, pos, color, direction, speed)
+
+    def _spawn_floating_particle(self):
+        init_pos = pygame.mouse.get_pos()
+        pos = init_pos[0] + randint(-10, 10), init_pos[1] + randint(-10, 10)
+        color = "white"
+        direction = pygame.math.Vector2(0, -1)
+        speed = randint(50, 100)
+        FloatingParticle(self.particle_group, pos, color, direction, speed)
