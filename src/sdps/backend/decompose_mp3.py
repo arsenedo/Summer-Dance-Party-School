dd90e424a25a3ad6e0f0c3b796f132387fc7e5c9
Arsenii <arsenii.nedorezov@gmail.com>
2026-08-21 13:11:24 +0200

    detect tempo and improve midi file generation to be detectable in fl studio

--- src/sdps/backend/decompose_mp3.py
+++ src/sdps/backend/decompose_mp3.py
@@ -1,3 +1,4 @@
+from mido import MidiTrack, MetaMessage
 
 from sdps.config import MP3_FILENAME
 import librosa
@@ -11,11 +12,12 @@ class DecomposeMp3:
         self.fmin = librosa.note_to_hz('C-2') #Note basse de la plage totale
         self.n_bins = 96 # Nombre de notes totales depuis la note basse
         self.notes = librosa.midi_to_note(librosa.hz_to_midi(librosa.cqt_frequencies(n_bins=self.n_bins, fmin=self.fmin)))
-        y, sr = librosa.load("./assets/sounds/" + MP3_FILENAME, sr=None)
+        y, sr = librosa.load("./assets/sounds/" + MP3_FILENAME, sr=None, duration=4, offset=7)
         self.y = y
         self.sr = sr
         self.notesTiming = librosa.onset.onset_detect(y=self.y, sr=self.sr, units='time')
         self.hop_length = 512
+        self.bpm = self.estimate_bmp()
         self.notes_midi = {
             "C": 60,
             "C♯": 61,
@@ -36,16 +38,18 @@ class DecomposeMp3:
     def run(self):
         # Création du fichier midi
         mid = mido.MidiFile()
+        self.create_meta_track(mid)
+
         track = mido.MidiTrack()
         mid.tracks.append(track)
-        tempo = 500000
+        tempo = mido.bpm2tempo(self.bpm, time_signature=(4, 4))
         last_message = 0.0
 
         # Calcul du CQT sur la plage définie
         C = librosa.cqt(self.y, sr=self.sr, hop_length=self.hop_length, fmin=self.fmin, n_bins=self.n_bins)
 
         for t in self.notesTiming:
-            print(t)
+            #print(t)
 
             target_time = t
             center_sample = int(target_time * self.sr)
@@ -59,7 +63,7 @@ class DecomposeMp3:
             # Détection des pic locaux
             peaks = scipy.signal.find_peaks(specific_db)
 
-            db = []
+            db = set([])
 
             # Recherche de l'id de la valeur max
             max_id = specific_db.argmax()
@@ -69,7 +73,7 @@ class DecomposeMp3:
             for p in peaks[0]:
                 if specific_db[p] > (specific_db[
                                          max_id] / 5):  # On garde les valeurs plus hautes que 20% de la note jouée la plus forte
-                    db.append(str(self.notes[p]).translate(str.maketrans("", "",
+                    db.add(str(self.notes[p]).translate(str.maketrans("", "",
                                                                     "0123456789-")))  # avec maketrans on supprimes les chiffres (On a pas besoin de garder l'octave)
             # print("notes trouvées = ")
             # print(set(db))
@@ -81,13 +85,26 @@ class DecomposeMp3:
             first_note = True
 
             for note in db:
-
                 if first_note:
                     track.append(mido.Message('note_on', note=self.notes_midi[note], velocity=64, time=ticks))
                     first_note = False
                 else:
                     track.append(mido.Message('note_on', note=self.notes_midi[note], velocity=64, time=0))
+                track.append(mido.Message("note_off", note=self.notes_midi[note], velocity=64, time=100))
 
         midi_fn = "new_song.mid"
         mid.save(midi_fn)
         print("Fichier midi créé et sauvegardé sous " + midi_fn)
+
+    def estimate_bmp(self):
+        onset_env = librosa.onset.onset_strength(y=self.y, sr=self.sr)
+        tempo = librosa.feature.tempo(onset_envelope=onset_env, sr=self.sr)
+        return tempo[0]
+
+    def create_meta_track(self, mid):
+        meta_track = MidiTrack()
+        mid.tracks.append(meta_track)
+        meta_track.append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(self.bpm), time=0))
+        meta_track.append(MetaMessage('time_signature', numerator=4, denominator=4, time=0))
+        meta_track.append(MetaMessage('end_of_track', time=0))
+
